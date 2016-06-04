e401bcffd55f506ee71a0f67011a80a0aa638c29
John <gtossk2@hotmail.com>
2016-06-04 02:31:15 -0700

    Debug for Mutex Problem

--- main.c
+++ main.c
@@ -4,11 +4,12 @@
 
 #define MAX_THREAD  2
 
-
-
+// Forward declaration
 typedef void *(*Func)(void* argc);
 typedef struct job Job;
 typedef struct jobqueue JobQueue;
+typedef struct threadPool ThreadPool;
+typedef struct thread_handler ThreadHandler;
 
 struct job{
   Job* next;
@@ -16,22 +17,42 @@ struct job{
   void *argc;
 };
 
+// TODO Put the jobqueue function inside structure
 struct jobqueue{
   Job *front;
   Job *rear;
   int job_number;
 };
 
-void *thread_handler(void *job);
+struct thread_handler{
+  ThreadPool *pool;
+  pthread_t thread;
+};
+
+struct threadPool{
+  ThreadHandler *thread;
+  int thread_number;
+  JobQueue *job_pool;
+};
+
+void threadPool_init(ThreadPool *threadPool, int thread_num);
+void threadPool_Add_job(ThreadPool *threadPool, Job *job);
+void threadPool_join(ThreadPool *threadPool);
+
+void threadHandler_init(ThreadPool *pool, ThreadHandler *thread);
+
+void *thread_handler(void *th_pool);
 void *hello(void *argc);
+
 void queue_push(JobQueue* queue, Job* newJob);
 Job* queue_pull(JobQueue* queue);
 
-JobQueue jobPool = {.front = NULL, .rear = NULL, .job_number = 0};
+//JobQueue jobPool = {.front = NULL, .rear = NULL, .job_number = 0};
+volatile int job_flag = 0;
+
 
 int main(){
-
-  pthread_t threads;
+//  pthread_t threads;
   int argc[5] = {1, 2, 3, 4, 5};
   Job job[5] = { {.handler = hello, .argc = &argc[0]},
                  {.handler = hello, .argc = &argc[1]},
@@ -39,7 +60,7 @@ int main(){
                  {.handler = hello, .argc = &argc[3]},
                  {.handler = hello, .argc = &argc[4]}
                };  
-
+/*
   int i = 0;
   for(i = 0; i < 5; ++i){
     queue_push(&jobPool, &job[i]);
@@ -49,7 +70,19 @@ int main(){
 
   pthread_join(threads, 0);
 
-  printf("Jon left : %d \n", jobPool.job_number);
+  printf("Job left : %d \n", jobPool.job_number);
+*/  
+
+  ThreadPool th_pool;
+  threadPool_init(&th_pool, 1);
+  int i = 0;
+  for(i = 0; i < 5; i++){
+    threadPool_Add_job(&th_pool, &job[i]);
+  }
+
+  printf("job : %d \n", th_pool.job_pool->job_number);
+  
+  threadPool_join(&th_pool);
   return 0;
 }
 
@@ -68,6 +101,8 @@ void queue_push(JobQueue* queue, Job* newJob){
   }
 
   queue->job_number++;
+  if(queue->job_number)
+    job_flag = 1;
 };
 
 Job* queue_pull(JobQueue* queue){
@@ -77,17 +112,67 @@ Job* queue_pull(JobQueue* queue){
   queue->front = first_job->next;
   queue->job_number--;
 
+  if(!queue->job_number)
+    job_flag = 0;
+
   return first_job;
 };
 
-void *thread_handler(void *job){
+void threadPool_init(ThreadPool *threadPool, int thread_num){
+ int idx = 0;
+
+ // Check threadPool do exist
+ if(threadPool == NULL)
+  return;
+
+ // Initial Thread_handler
+  if(thread_num < 1)
+    thread_num = 1;
+
+  threadPool->thread = malloc(sizeof(thread_handler) * thread_num);
+  for(idx = 0; idx < thread_num; idx++){
+    threadHandler_init(threadPool, &threadPool->thread[idx]);
+  }
+
+  // Initial Job Queue
+  threadPool->job_pool = malloc(sizeof(JobQueue));
+};
+
+void threadPool_Add_job(ThreadPool *threadPool, Job *job){
+  queue_push(threadPool->job_pool, job);
+};
+
+void threadHandler_init(ThreadPool *pool, ThreadHandler *thread){
+  printf("Hello");
+  thread->pool = pool;
+
+  printf("thread : %p \n", &thread->thread);
+  pthread_create(&thread->thread, NULL, &thread_handler, (void *)pool);
+};
+
+void *thread_handler(void *th_pool){
   Job* t;
-
+  ThreadPool *threadPool = (ThreadPool *)th_pool;
+  printf("test");
   do{
-    t = queue_pull(&jobPool);
-    t->handler(t->argc);
-  }while(jobPool.job_number);
+    if(job_flag){    
+      t = queue_pull(threadPool->job_pool);
+      t->handler(t->argc);
+    } else {
+      printf("Thread idle ... \n");
+    }
+      
+  }while(1);
+}
 
+void threadPool_join(ThreadPool *threadPool){
+  int idx = 0;
+  ThreadHandler *th_handler;
+
+  for(idx = 0; idx < threadPool->thread_number; idx++){
+    th_handler = &threadPool->thread[idx];
+    pthread_join(th_handler->thread, 0);
+  }
 }
 
 void *hello(void *argc){
